f2dc4252c2a229a47fb82e9726296f8bcd3d20f4
Riya kaushik <riyakaushik6410@gmail.com>
2025-10-18 13:56:22 +0530

    initial setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+     <div>Welcome to TaskPulse - A full-stack task management application built with MERN (MongoDB, Express, React, Node.js), designed for efficient workflow, productivity tracking, and seamless task organization. Features include user authentication, real-time task updates, priority management, and responsive UI.</div>
+    </>
+  )
+}
+
+export default App
