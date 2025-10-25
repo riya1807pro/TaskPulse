3503cfcce13ccfea6a00a2742bb1577249f3664a
Riya kaushik <riyakaushik6410@gmail.com>
2025-10-25 13:18:24 +0530

    update user profile api route

---
+++ backend/controllers/auth.controllers.js
@@ -0,0 +1,128 @@
+import  User from "../models/user.modals.js"
+import bcryptjs from "bcryptjs"
+import { ErrorHandler } from "../utils/error.js"
+import jwt from "jsonwebtoken"
+
+export const signUp = async (req, res, next) => {
+  const { name, email, password, profileImageUrl, adminJoinCode } = req.body
+
+  if (
+    !name ||
+    !email ||
+    !password ||
+    name === "" ||
+    email === "" ||
+    password === ""
+  ) {
+    return next(ErrorHandler(400, "All fields are required"))
+  }
+
+  //   Check if user already exists
+  const isAlreadyExist = await User.findOne({ email })
+
+  if (isAlreadyExist) {
+    return next(ErrorHandler(400, "User already exists"))
+  }
+
+  //   check user role
+  let role = "user"
+
+  if (adminJoinCode && adminJoinCode === process.env.ADMIN_JOIN_CODE) {
+    role = "admin"
+  }
+
+  const hashedPassword = bcryptjs.hashSync(password, 10)
+
+  const newUser = new User({
+    name,
+    email,
+    password: hashedPassword,
+    profileImageUrl,
+    role,
+  })
+
+  try {
+    await newUser.save()
+
+    res.json("Signup successful")
+  } catch (error) {
+    next(error.message)
+  }
+}
+
+export const SignIn = async (req, res, next) => {
+  try {
+    const { email, password } = req.body
+
+    if (!email || !password || email === "" || password === "") {
+      return next(ErrorHandler(400, "All fields are required"))
+    }
+
+    const validUser = await User.findOne({ email })
+
+    if (!validUser) {
+      return next(ErrorHandler(404, "User not found!"))
+    }
+
+    // compare password
+    const validPassword = bcryptjs.compareSync(password, validUser.password)
+
+    if (!validPassword) {
+      return next(ErrorHandler(400, "Wrong Credentials"))
+    }
+
+    const token = jwt.sign(
+      { id: validUser._id, role: validUser.role },
+      process.env.JWT_SECRET
+    )
+
+    const { password: pass, ...rest } = validUser._doc
+
+    res.status(200).cookie("access_token", token, { httpOnly: true }).json(rest)
+  } catch (error) {
+    next(error)
+  }
+}
+
+export const userProfile = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.user.id)
+
+    if (!user) {
+      return next(ErrorHandler(404, "User not found!"))
+    }
+
+    const { password: pass, ...rest } = user._doc
+
+    res.status(200).json(rest)
+  } catch (error) {
+    next(error)
+  }
+}
+
+export const updateUserProfile = async (req, res, next) => {
+  try {
+    const user = req.User.id;
+
+    if(!user){
+      return next(ErrorHandler(404, "User not found!"))
+    }
+
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+    user.profileImageUrl = req.body.profileImageUrl || user.profileImageUrl;
+
+    if(req.body.password){
+      user.password = bcryptjs.hashSync(req.body.password, 10);
+    }
+
+    const updatedUser = await user.save();
+
+    const {password:pass , ...rest} = updatedUser._doc;
+
+    res.status(200).json(rest);
+
+  } catch (error) {
+    
+  }
+}
--- backend/routes/auth.router.js
+++ backend/routes/auth.router.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { SignIn, signUp, userProfile } from '../controllers/auth.controolers.js';
+import { SignIn, signUp, updateUserProfile, userProfile } from '../controllers/auth.controllers.js';
 import { verifyUser } from '../utils/verifyUser.js';
 
 const router = express.Router();
@@ -11,4 +11,5 @@ router.post("/signIn", SignIn);
 router.post("/userProfile", verifyUser , userProfile);
 router.get("/userProfile", verifyUser , userProfile);
 
+router.put("/updateUserProfile", verifyUser , updateUserProfile);
 export default router;
